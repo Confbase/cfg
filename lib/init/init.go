b92cf0dc70db7c326d06473fc56ef45cc3e90f42
Thomas Fischer <thomas@confbase.com>
2018-04-06 14:51:11 -0500

    init: use values from global config file

--- lib/init/init.go
+++ lib/init/init.go
@@ -7,10 +7,12 @@ import (
 	"os"
 	"path"
 
+	"github.com/spf13/viper"
+
 	"github.com/confbase/cfg/lib/dotcfg"
 )
 
-func Init() {
+func Init(append, overwrite bool) {
 	cwd, err := os.Getwd()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: failed to get working directory\n")
@@ -20,14 +22,14 @@ func Init() {
 	dotCfgDirPath := mkDotCfgDir(cwd)
 	mkDotfile(cwd)
 	mkKeyfile(dotCfgDirPath)
-	mkGitIgnore(cwd)
+	mkGitIgnore(cwd, append, overwrite)
 
 	fmt.Printf("Initialized empty base in %v\n", dotCfgDirPath)
 }
 
 func mkDotCfgDir(baseDir string) string {
 	dirPath := path.Join(baseDir, dotcfg.Dirname)
-	notExistErrOut(dirPath)
+	notExistErrOut(dirPath, "")
 
 	if err := os.Mkdir(dirPath, 0755); err != nil {
 		fmt.Fprintf(os.Stderr, "error: failed to create directory %v\n", dirPath)
@@ -38,11 +40,7 @@ func mkDotCfgDir(baseDir string) string {
 
 func mkDotfile(baseDir string) {
 	filePath := path.Join(baseDir, dotcfg.FileName)
-	_, err := os.Stat(filePath)
-	if err == nil || (err != nil && !os.IsNotExist(err)) {
-		fmt.Fprintf(os.Stderr, "error: %v already exists or stat() failed\n", filePath)
-		os.Exit(1)
-	}
+	notExistErrOut(filePath, "")
 
 	dotfile := dotcfg.File{
 		Templates: make([]string, 0),
@@ -62,9 +60,12 @@ func mkDotfile(baseDir string) {
 
 func mkKeyfile(baseDir string) {
 	filePath := path.Join(baseDir, dotcfg.KeyfileName)
-	notExistErrOut(filePath)
+	notExistErrOut(filePath, "")
 
-	keyfile := dotcfg.Key{}
+	keyfile := dotcfg.Key{
+		Email:      viper.GetString("email"),
+		EntryPoint: viper.GetString("entryPoint"),
+	}
 	data, err := json.Marshal(keyfile)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: failed to serialize %v\n", filePath)
@@ -77,20 +78,40 @@ func mkKeyfile(baseDir string) {
 	}
 }
 
-func mkGitIgnore(baseDir string) {
+func mkGitIgnore(baseDir string, append, overwrite bool) {
 	filePath := path.Join(baseDir, ".gitignore")
-	notExistErrOut(filePath)
-	ignoreBytes := []byte(".cfg/\n")
-	if err := ioutil.WriteFile(filePath, ignoreBytes, 0644); err != nil {
+	ignoreStr := ".cfg/\n"
+
+	if append {
+		f, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to open .gitignore for appending\n")
+			os.Exit(1)
+		}
+		defer f.Close()
+
+		if _, err = f.WriteString(ignoreStr); err != nil {
+			fmt.Fprintf(os.Stderr, "failed to write to .gitignore\n")
+			os.Exit(1)
+		}
+		return
+	}
+
+	if !overwrite {
+		notExistErrOut(filePath, "did you mean to use --overwrite-gitignore or --append-to-gitignore?\n")
+	}
+
+	if err := ioutil.WriteFile(filePath, []byte(ignoreStr), 0644); err != nil {
 		fmt.Fprintf(os.Stderr, "error: failed to write to %v\n", filePath)
 		os.Exit(1)
 	}
 }
 
-func notExistErrOut(filePath string) {
+func notExistErrOut(filePath, msg string) {
 	_, err := os.Stat(filePath)
 	if err == nil || (err != nil && !os.IsNotExist(err)) {
-		fmt.Fprintf(os.Stderr, "error: %v already exists or stat() failed\n", filePath)
+		fmt.Fprintf(os.Stderr, "error: %v already exists\n", filePath)
+		fmt.Fprintf(os.Stderr, "%v", msg)
 		os.Exit(1)
 	}
 }
